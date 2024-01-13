0680450d7201eb4a5090d54dbd61bb923f082559
CoreyZhong <coreyzhong@gmail.com>
2024-01-12 18:00:44 -0800

    creating intake file

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Add your docs here. */
+public class Intake extends SubsystemBase{
+    private CANSparkMax pivotIntake;
+}
