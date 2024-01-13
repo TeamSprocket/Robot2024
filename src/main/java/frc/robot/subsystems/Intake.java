8155d085c105f7a25d5aca47f215ff3942801767
CoreyZhong <coreyzhong@gmail.com>
2024-01-12 18:11:10 -0800

    new intake info

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,11 +4,18 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** Add your docs here. */
 public class Intake extends SubsystemBase{
-    private CANSparkMax pivotIntake;
+    private TalonFX pivotIntake;
+    private TalonFX rollIntake;
+
+    public Intake() {
+        
+    }
 }
