5e939c19c0bd58e6ebd459437fb55e2f53a900c1
Emily Luo <139726246+emilyhjluo@users.noreply.github.com>
2024-01-15 11:49:47 -0800

    Update Elevator.java

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -5,24 +5,23 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
-  motorLeft, motorRight;
-  motorRight.follow(motorLeft);
-leftMotor.set(0.5);
 
   private final WPI_TalonFX motorLeft = new WPI_TalonFX(0);
   private final WPI_TalonFX motorRight = new WPI_TalonFX(0);
   /** Creates a new elevator. */
-  public Elevator() {}
+  public Elevator() {
+    motorRight.follow(motorLeft);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
   public void goingup(){
-    motorLeft.set(40);
+    motorLeft.set(0.5);
   }
   public void goingdown(){
-    motorLeft.set(-1);
+    motorLeft.set(-0.5);
   }
   public void stop(){
     motorLeft.set(0);
