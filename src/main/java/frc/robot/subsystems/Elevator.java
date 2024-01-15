a94513e72832c6d2b1f34c12d7486fa8d43c1c85
Emily Luo <139726246+emilyhjluo@users.noreply.github.com>
2024-01-15 13:20:27 -0800

    Update Elevator.java

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,15 +6,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
 
-  private final WPI_TalonFX motorLeft = new WPI_TalonFX(0);
-  private final WPI_TalonFX motorRight = new WPI_TalonFX(0);
-  /** Creates a new elevator. */
+  public static enum ElevatorStates {
+    NONE,
+    STOWED,
+    HANDOFF,
+    SPEAKER,
+    SPEAKER_HIGH,
+    AMP,
+    MANUAL
+  }
+
+  private ElevatorStates state = ElevatorStates.NONE;
+  private ElevatorStates lastState = ElevatorStates.NONE;
+
+  private WPI_TalonFX motorLeft = new WPI_TalonFX(0);
+  private WPI_TalonFX motorRight = new WPI_TalonFX(0);
+
+  WPI_TalonFX motorLeft = new WPI_TalonFX(RobotMap.Elevator.Left);
+  WPI_TalonFX motorRight = new WPI_TalonFX(RobotMap.ELEVATOR.Right);
+
+  PIDController motorLeft = new PIDController(Constants.Elevator.kPElevator, Constants.Shooter.kIElevator, Constants.Elevator.kDLeft);
+  PIDController motorRight = new PIDController(Constants.Elevator.kPIndexer, Constants.Shooter.kIElevator, Constants.Elevator.kDRight);
+  
   public Elevator() {
     motorRight.follow(motorLeft);
   }
 
   @Override
   public void periodic() {
+    swtich (state) {
+      case NONE:
+        motorLeft.set(0)
+        
     // This method will be called once per scheduler run
   }
   public void goingup(){
