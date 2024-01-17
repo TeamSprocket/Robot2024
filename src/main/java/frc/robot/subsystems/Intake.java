d9df6424423f7dcfbabe2f376aa6a1d35d0b62e9
Karma1-1-1-1 <jai.kank01@gmail.com>
2024-01-17 08:45:12 -0800

    Roll Intake
    Under intake state

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,12 +6,22 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+import frc.util.ShuffleboardPIDTuner;
 
 /** Add your docs here. */
-public class Intake extends SubsystemBase{
+public class Intake extends SubsystemBase {
+
+    private final WPI_TalonFX claw = new WPI_TalonFX(RobotMap.Claw.CLAW);
+    double idleSpeed = 0;
+    double activeSpeed = 0;
 
     public enum IntakeState {
         NONE,
@@ -29,6 +39,31 @@ public class Intake extends SubsystemBase{
     public Intake() {
 
         intakeState = IntakeState.NONE;
+        claw.setInverted(false);
+        claw.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 50, 50, 1.0));
+        claw.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 50, 50, 1.0));
+
+        ShuffleboardPIDTuner.addSlider("kIdleSpeed", 0, 1, 0.1);
+
+        claw.setNeutralMode(NeutralMode.Brake);
+    }
+
+    public void moveClaw(double output) {
+        if (output > 0) {
+            idleSpeed = ShuffleboardPIDTuner.get("kIdleSpeed");
+        } else if (output < 0) {
+            idleSpeed = -ShuffleboardPIDTuner.get("kIdleSpeed");
+        }
+
+        activeSpeed = idleSpeed + output;
+    }
+
+    public void clearStickyFaults() {
+        rollIntake.clearStickyFaults();
+    }
+
+    public double getVelocity() {
+        return claw.getSelectedSensorVelocity();
     }
 
     @Override
@@ -42,6 +77,8 @@ public class Intake extends SubsystemBase{
 
                 break;
             case INTAKE:
+                claw.set(ControlMode.PercentOutput, activeSpeed);
+                SmartDashboard.putNumber("[Claw] RPM", getVelocity());
 
                 break;
             case WAIT_HANDOFF:
