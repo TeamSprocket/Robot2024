0db025f3f38ca1a021ab3543b42d3f8961037a16
Zevzzz <46076391+Zevzzz@users.noreply.github.com>
2024-01-22 10:47:53 -0800

    PP Translation constants, Untuned Rotation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -77,11 +77,11 @@ public class Constants {
         public static final double kIHeading = 0.0000;
         public static final double kDHeading = 0.12136; 
 
-        public static final double kPTranslationPP = 0.05; 
+        public static final double kPTranslationPP = 2.7; 
         public static final double kITranslationPP = 0.0;
         public static final double kDTranslationPP = 0.0; 
 
-        public static final double kPRotationPP = 0.01; 
+        public static final double kPRotationPP = 1.0; 
         public static final double kIRotationPP = 0.0;
         public static final double kDRotationPP = 0.0;
 
