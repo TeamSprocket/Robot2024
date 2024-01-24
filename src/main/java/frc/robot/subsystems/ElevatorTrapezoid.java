c2f91f1da50726617437fcc4c1d86f8478e56f58
04041b <65881086+04041b@users.noreply.github.com>
2024-01-23 17:12:41 -0800

    update elevator trapezoid

--- src/main/java/frc/robot/subsystems/ElevatorTrapezoid.java
+++ src/main/java/frc/robot/subsystems/ElevatorTrapezoid.java
@@ -6,11 +6,14 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import java.util.function.Supplier;
 
+import org.apache.commons.lang3.Conversion;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
+import frc.util.Conversions;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
@@ -20,48 +23,25 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.TrapezoidProfileSubsystem;
 
-public class ElevatorTrapezoid extends TrapezoidProfileSubsystem {
+
+//https://docs.wpilib.org/en/stable/docs/software/advanced-controls/controllers/trapezoidal-profiles.html
+public class ElevatorTrapezoid extends SubsystemBase {
 
 
   private WPI_TalonFX motorLeft = new WPI_TalonFX(RobotMap.Elevator.Left);
   private WPI_TalonFX motorRight = new WPI_TalonFX(RobotMap.Elevator.Right);  
-  
-  public void follow() {
+    PIDController pidControllerLeft = new PIDController(Constants.Elevator.kPElevator, Constants.Elevator.kIElevator, Constants.Elevator.kDElevator);
+    TrapezoidProfile profile = new TrapezoidProfile(new TrapezoidProfile.Constraints(1, 1));
+
+  public ElevatorTrapezoid() {
     motorRight.follow(motorLeft);
   }
-  
 
-private final ArmFeedforward m_feedforward =
-      new ArmFeedforward(
-          0.01, 0.01,
-          0.01, 0.01);
-
-  PIDController pidControllerLeft = new PIDController(Constants.Elevator.kPElevator, Constants.Elevator.kIElevator, Constants.Elevator.kDElevator);
-  
-
-  public ElevatorTrapezoid() {super(
-new TrapezoidProfile.Constraints(
-            1, 0.5),
-        0.2);
-  
+  @Override
+  public void periodic() {
+    State trapState = profile.calculate(5, new TrapezoidProfile.State(0, 0), new TrapezoidProfile.State(1, 0));
+    pidControllerLeft.calculate(motorLeft.getSelectedSensorPosition(),trapState.position);
   }
-
-
-  public void useState(TrapezoidProfile.State setpoint) {
-    // Calculate the feedforward from the sepoint
-    double feedforward = m_feedforward.calculate(setpoint.position, setpoint.velocity);
-    // Add the feedforward to the PID output to get the motor output
-    motorLeft.setSetpoint(
-        
-        (motorLeft.getSelectedSensorPosition(), setpoint.position, feedforward / 12.0));
-  }
-
-  public Command setArmGoalCommand(double kArmOffsetRads) {
-    return Commands.runOnce(() -> setGoal(kArmOffsetRads), this);
-  }
-
-
-
 
   
 }
