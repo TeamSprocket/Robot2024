c01defeae6fefd3d531db034385165f67cb35c22
birbbbbbbie <allyhoang27@gmail.com>
2024-01-24 19:03:12 -0800

    added debug

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.util.Conversions;
@@ -121,5 +122,9 @@ public class Wrist extends SubsystemBase {
 
   public void setState(WristStates wristStates) {
     this.wristStates = wristStates;
+  }
+
+  public void debug() {
+    SmartDashboard.putNumber("Angle in Degrees", getAngleofMotor());
   }
 }
