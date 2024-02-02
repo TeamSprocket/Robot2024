e20aafa7cbcaf641ec97140d79b610bc21a135e5
Eric Wang <108838192+sdkjg@users.noreply.github.com>
2024-02-01 17:17:26 -0800

    volatility method to filter limelight for pathplanner

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -9,11 +11,28 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.SwerveDrive;
+import frc.robot.Constants;
 
 public class Limelight extends SubsystemBase {
 
     private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     private MedianFilter filter = new MedianFilter(10);
+
+    private ArrayList<Double> llOdometryX = new ArrayList<Double>(50);
+    private ArrayList<Double> llOdometryY = new ArrayList<Double>(50);
+
+    private double totalX = 0.00;
+    private double totalY = 0.00;
+
+    private double averageX = 0.00;
+    private double averageY = 0.00;
+
+    private final double limeLightDeviation = 0.75;
+
+    SwerveDrive swerveDrive;
+
+
     // TODO: test with only one filter because I think we only need one
 
     public Limelight() {}
@@ -24,6 +43,21 @@ public class Limelight extends SubsystemBase {
         SmartDashboard.putNumber("Pose Y", table.getEntry("ty").getDouble(0.0));
         SmartDashboard.putNumber("Filtered X", getTranslation2d().getX());
         SmartDashboard.putNumber("Filtered Y", getTranslation2d().getY());
+
+        totalX += getTranslation2d().getX();
+        totalX -= llOdometryX.get(0);
+        averageX = totalX / 50;
+
+        llOdometryX.add(getTranslation2d().getX());
+        llOdometryX.remove(0);
+
+        totalY += getTranslation2d().getY();
+        totalY -= llOdometryY.get(0);
+        averageY = totalY / 50;
+
+        llOdometryY.add(getTranslation2d().getY());
+        llOdometryY.remove(0);
+        
     }
 
     public Translation2d getTranslation2d() {
@@ -44,4 +78,27 @@ public class Limelight extends SubsystemBase {
 
         return (new Translation2d(filteredX, filteredY));
     }
+
+    public void updateOdometry() {
+        double volatilityX = getVolatility(averageX, llOdometryX);
+        double volatilityY = getVolatility(averageY, llOdometryY);
+
+        if (volatilityX <= limeLightDeviation && volatilityY <= limeLightDeviation){
+            swerveDrive.updateOdometryWithVision();
+        } else {
+            swerveDrive.getPose();
+        }
+
+
+    }
+    
+    public double getVolatility(double average, ArrayList llOdometry){
+        double volatility = 0.00;
+        for(int i = 0; i <50; i++){
+            volatility += Math.abs(average - (double)llOdometry.get(i));
+        }
+        return volatility;
+    }
+
+
 }
--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -283,8 +283,7 @@ public class SwerveDrive extends SubsystemBase {
    * @return where the robot is according to odometry
    */
   public Pose2d getPose() {
-    return new Pose2d(limelight.getTranslation2d().getX(), limelight.getTranslation2d().getY(), new Rotation2d(getHeading()));
-    //return odometry.getPoseMeters();
+        return odometry.getPoseMeters();
   }
 
   /**
