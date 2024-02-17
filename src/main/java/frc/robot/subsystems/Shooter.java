508c369e4e6f63bc1ba69c6312e11877926117cd
z57333 <73370638+z57333@users.noreply.github.com>
2024-02-17 11:30:19 -0800

    minor change to shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -141,7 +141,7 @@ public class Shooter extends SubsystemBase {
   }
 
   public double getShooterRPS() {
-    double shooterVelocityCounts = shooterMotor.getPosition().getValueAsDouble();
+    double shooterVelocityCounts = shooterMotor.getVelocity().getValueAsDouble();
     double rpm = Conversions.falconToRPM(shooterVelocityCounts, Constants.Shooter.kShooterGearRatio);
     double rps = rpm / 60.0;
     return rps;
