af34174672bd889ef5705785d64bba55edc9355b
Zevzzz <46076391+Zevzzz@users.noreply.github.com>
2024-02-29 15:52:39 -0800

    Fixed import error

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -29,7 +29,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.util.CTREUtils;
+// import frc.util.CTREUtils;
 import frc.util.Conversions;
 import frc.util.ShuffleboardPIDTuner;
 
