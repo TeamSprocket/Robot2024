e93fbd3a4972c416bfbb89b28e9cc3664ab83931
birbbbbbbie <116534973+birbbbbbbie@users.noreply.github.com>
2024-03-05 15:22:11 -0800

    silly sim things

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -28,7 +28,7 @@ public class Vision extends SubsystemBase {
     private Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Cam mounted facing forward, half a meter forward of center, half a meter up from center.
     private PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.LOWEST_AMBIGUITY, shooterLL, robotToCam);
 
-    private PhotonTrackedTarget target;
+    private PhotonTrackedTarget target = new PhotonTrackedTarget();
     private SendableChooser<Boolean> cameraMode = new SendableChooser<Boolean>();
 
     private ArrayList<Double> xPoseReadings = new ArrayList<Double>(50);
