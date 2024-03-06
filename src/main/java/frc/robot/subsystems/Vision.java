d4225b5532a3f000e46ee4bf0f525e606dc70b5a
birbbbbbbie <116534973+birbbbbbbie@users.noreply.github.com>
2024-03-06 12:33:29 -0800

    :DDD

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -26,7 +26,7 @@ public class Vision extends SubsystemBase {
     private PhotonCamera intakeLL = new PhotonCamera("CAMERA NAME");
     
     private AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-    private Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Cam mounted facing forward, half a meter forward of center, half a meter up from center.
+    private Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Cam mounted facing forward, half a meter forward of center, half a meter up from center // TODO: change transform based on cam mounting
     private PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.LOWEST_AMBIGUITY, shooterLL, robotToCam);
 
     private PhotonTrackedTarget target;
