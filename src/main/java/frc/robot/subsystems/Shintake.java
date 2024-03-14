c8c98a83a3621d8ba0f36e628fbf87588d3fb624
z57333 <73370638+z57333@users.noreply.github.com>
2024-03-13 17:09:19 -0700

    stuff

--- src/main/java/frc/robot/subsystems/Shintake.java
+++ src/main/java/frc/robot/subsystems/Shintake.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.controller.PIDController;
@@ -36,14 +37,8 @@ public class Shintake extends SubsystemBase {
 
     ShintakeStates state;
 
-    private final TalonFX shintakeMotor = new TalonFX(RobotMap.Intake.ROLL_INTAKE); // TODO: Remember that theres 2
-                                                                                    // motors for intake, a front and an
-                                                                                    // inner one, make second motor and
-                                                                                    // make sure youre using the right
-                                                                                    // one
-    private final TalonFX indexerMotor = new TalonFX(RobotMap.Shooter.INDEXER); // TODO: (I'd call the outer roller
-                                                                                // "ShshintakeMotor", and inner one
-                                                                                // "IndexerMotor")
+    private final TalonFX shintakeMotor = new TalonFX(RobotMap.Intake.ROLL_INTAKE); 
+    private final TalonFX indexerMotor = new TalonFX(RobotMap.Shooter.INDEXER); 
     private final TalonFX pivotMotor = new TalonFX(RobotMap.Intake.PIVOT_INTAKE);
 
     // private double pivotSpeed = 0;
@@ -74,6 +69,12 @@ public class Shintake extends SubsystemBase {
         stateChooser.addOption("SCORE_AMP", ShintakeStates.SCORE_AMP);
 
         SmartDashboard.putData("Intake State", stateChooser);
+
+        ShuffleboardPIDTuner.addSlider("PID kP pivot", 0, 1, 0);
+        ShuffleboardPIDTuner.addSlider("PID kD pivot", 0, 1, 0);
+
+        pidControllerIntake.setP(ShuffleboardPIDTuner.get("PID kP pivot"));
+        pidControllerIntake.setD(ShuffleboardPIDTuner.get("PID kD pivot"));
     }
 
     @Override
