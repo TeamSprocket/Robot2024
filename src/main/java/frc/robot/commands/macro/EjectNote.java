ae37c5e44e5c5c478526f103f796127899bab236
z57333 <73370638+z57333@users.noreply.github.com>
2024-03-13 17:11:00 -0700

    minor stuff

--- src/main/java/frc/robot/commands/macro/EjectNote.java
+++ src/main/java/frc/robot/commands/macro/EjectNote.java
@@ -2,29 +2,24 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.macro;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Intake.IntakeStates;
-import frc.robot.subsystems.Shooter.ShooterStates;
+import frc.robot.subsystems.Shintake;
+import frc.robot.subsystems.Shintake.ShintakeStates;
 
 public class EjectNote extends Command {
   /** Creates a new EjectIntake. */
-  Intake intake;
-  Shooter shooter;
-  public EjectNote(Intake intake, Shooter shooter) {
-    this.intake = intake;
-    this.shooter = shooter;
+  Shintake shintake;
+  public EjectNote(Shintake shintake) {
+    this.shintake = shintake;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intake.setState(IntakeStates.EJECT_NOTE);
-    shooter.setState(ShooterStates.EJECT_NOTE);
+    shintake.setState(ShintakeStates.EJECT_NOTE);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -34,13 +29,12 @@ public class EjectNote extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intake.setState(IntakeStates.STOWED);
-    shooter.setState(ShooterStates.STANDBY);
+    shintake.setState(ShintakeStates.STOWED);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return (!shintake.hasNote());
   }
 }
