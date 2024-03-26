e93d016d5c9f371da2bd7659121a91ee9027285f
Emily Luo <139726246+emilyhjluo@users.noreply.github.com>
2024-03-25 22:16:32 -0700

    03.25.2024

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -38,7 +38,7 @@ public class Robot extends TimedRobot {
   public void disabledInit() {
     Constants.robotState = RobotState.DISABLED;
     m_robotContainer.getSwerveDrive().setNeutralMode(NeutralModeValue.Coast);
-    m_robotContainer.getShintake().setNeutralMode(NeutralModeValue.Coast);
+    //m_robotContainer.getShintake().setNeutralMode(NeutralModeValue.Coast);
     
   }
 
--- src/main/java/frc/robot/commands/macro/ShootAmp.java
+++ src/main/java/frc/robot/commands/macro/ShootAmp.java
@@ -4,13 +4,21 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shintake;
 import frc.robot.subsystems.Shintake.ShintakeStates;
 
+/*
+ * initialize Shintake
+ */
+
 public class ShootAmp extends Command {
     Shintake shintake = new Shintake();
     public ShootAmp(Shintake shintake){
         this.shintake = shintake;
     }
 
-  // Called when the command is initially scheduled.
+// Called when the command is initially scheduled.
+  /**
+   * when started, the command will call SCORE_AMP
+   * SCORE_AMP is 
+   */
   @Override
   public void initialize() {
     shintake.setState(ShintakeStates.SCORE_AMP);
@@ -21,12 +29,18 @@ public class ShootAmp extends Command {
   public void execute() {}
 
   // Called once the command ends or is interrupted.
+  /**
+   * once something happens, the instake will be at the stowed state
+   */
   @Override
   public void end(boolean interrupted) {
     shintake.setState(ShintakeStates.STOWED);
   }
 
   // Returns true when the command should end.
+  /**
+   * finished
+   */
   @Override
   public boolean isFinished() {
     return (!shintake.hasNote());
