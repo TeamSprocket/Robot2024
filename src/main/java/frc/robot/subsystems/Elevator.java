5c369d7062c6b7c7763770716e1b2394ca37f9e2
z57333 <73370638+z57333@users.noreply.github.com>
2024-04-04 21:29:32 -0700

    Motion Magic Implementation

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,6 +4,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import java.util.function.Supplier;
 
@@ -24,6 +25,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.TrapezoidProfileSubsystem;
 
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+
 public class Elevator extends SubsystemBase {
   Supplier <Double> joyvalue;
   double output;
@@ -39,17 +46,31 @@ public class Elevator extends SubsystemBase {
 
   private TalonFX elevatorMotor = new TalonFX(RobotMap.Elevator.ELEVATOR_LEFT);
   private TalonFX elevatorFollowerMotor = new TalonFX(RobotMap.Elevator.ELEVATOR_RIGHT);
+  private MotionMagicVoltage mmV = new MotionMagicVoltage(0);
 
   PIDController pidController = new PIDController(Constants.Elevator.kPIDElevator.kP, Constants.Elevator.kPIDElevator.kI, Constants.Elevator.kPIDElevator.kD);
 
   SendableChooser<ElevatorStates> stateChooser = new SendableChooser<ElevatorStates>();
+  TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
 
+  
   
   public Elevator() {
     elevatorFollowerMotor.setControl(new StrictFollower(elevatorMotor.getDeviceID()));
 
     elevatorMotor.setInverted(Constants.Elevator.kLeftMotorIsInverted);
     elevatorFollowerMotor.setInverted(Constants.Elevator.kRightMotorIsInverted);
+
+    elevatorMotor.setNeutralMode(NeutralModeValue.Brake);
+    elevatorFollowerMotor.setNeutralMode(NeutralModeValue.Brake);
+
+    MotionMagicConfigs mm = talonFXConfigs.MotionMagic;
+    mm = mm.withMotionMagicCruiseVelocity(10);
+    mm = mm.withMotionMagicAcceleration(5);
+    mm = mm.withMotionMagicJerk(50);
+    
+    elevatorMotor.getConfigurator().apply(talonFXConfigs);
+    elevatorFollowerMotor.getConfigurator().apply(talonFXConfigs);
 
     ShuffleboardPIDTuner.addSlider("Elevator kP [EL]", 0, 1, Constants.Elevator.kPIDElevator.kP);
     ShuffleboardPIDTuner.addSlider("Elevator kD [EL]", 0, 1, Constants.Elevator.kPIDElevator.kD);
@@ -61,7 +82,7 @@ public class Elevator extends SubsystemBase {
     stateChooser.addOption("AMP", ElevatorStates.AMP);
     stateChooser.addOption("Manual", ElevatorStates.MANUAL);
 
-    SmartDashboard.putData("Shooter State Chooser [ST]", stateChooser);
+    SmartDashboard.putData("Elevator State Chooser [EL]", stateChooser);
   }
 
 
@@ -71,9 +92,7 @@ public class Elevator extends SubsystemBase {
     setState(stateChooser.getSelected());
     
     pidController.setP(ShuffleboardPIDTuner.get("Elevator kP [EL]"));
-    pidController.setD(ShuffleboardPIDTuner.get("Elevator kP [EL]"));
-    pidController.setD(ShuffleboardPIDTuner.get("Elevator kFF [EL]"));
-
+    pidController.setD(ShuffleboardPIDTuner.get("Elevator kD [EL]"));
 
     switch (state) {
         
@@ -103,11 +122,9 @@ public class Elevator extends SubsystemBase {
   }
 
   public void moveToHeight(double targetHeight) {
-    pidController.setSetpoint(targetHeight);
-    double currentHeight = getHeight();
-
-    double motorOutput = pidController.calculate(currentHeight);
-    motorOutput += Constants.Elevator.kPIDElevator.kFF;
+    double motorOutput = pidController.calculate(getHeight(), targetHeight);
+    //motorOutput += Constants.Elevator.PIDTuner.kFF;
+    motorOutput += ShuffleboardPIDTuner.get("Elevator kFF [EL]");
     motorOutput = Util.minmax(motorOutput, -1.0 * Constants.Elevator.kElevatorMotorMaxOutput, Constants.Elevator.kElevatorMotorMaxOutput);
     elevatorMotor.set(motorOutput);
   }
