8960994809e34d9b3abf54f281cd72de93a79437
birbbbbbbie <116534973+birbbbbbbie@users.noreply.github.com>
2024-07-16 16:09:00 -0700

    state based updates

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,53 +1,37 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
-
-import java.sql.Driver;
-import java.util.function.Supplier;
-
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.*;
-import frc.robot.subsystems.Shooter.ShooterStates;
 import frc.robot.subsystems.Superstructure.SSStates;
 import frc.robot.subsystems.swerve.TunerConstants;
 import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
 // import frc.robot.subsystems.swerve.Telemetry;
-
 public class RobotContainer {
-
-  // public final static Controller driver = new Controller(0);
   private final CommandXboxController driver = new CommandXboxController(0); // My joystick
-  // public final static Controller operator = new Controller(1);
   private final CommandXboxController operator = new CommandXboxController(1);
 
   PowerDistribution pdh = new PowerDistribution();
-
   private final TunerConstants tunerConst = new TunerConstants();
-  private final CommandSwerveDrivetrain drivetrain = tunerConst.DriveTrain; // My drivetrain
-  // private final Telemetry logger = new Telemetry(Constants.Drivetrain.MaxSpeed);
-  
+  private final CommandSwerveDrivetrain drivetrain = tunerConst.DriveTrain; // My drivetrain  
   private final ShooterPivot shooterPivot = new ShooterPivot(() -> operator.getLeftY(), () -> drivetrain.getTranslation3d());
   private final Shooter shooter = new Shooter(() -> drivetrain.getPose().getTranslation(), () -> operator.getRightTriggerAxis(), () -> operator.getLeftTriggerAxis(), () -> drivetrain.getTranslation3d());
   private final Intake intake = new Intake();
-
   Superstructure superstructure = new Superstructure(shooterPivot, shooter, intake);
 
   // ------- Swerve Generated -------
@@ -57,105 +41,73 @@ public class RobotContainer {
       .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric driving in open loop
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
   private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-
   public SendableChooser<Command> autonChooser = new SendableChooser<Command>();
 
   public RobotContainer() {
     configureBindings();
   }
-
+  
   public Command getAutonomousCommand() {
     return autonChooser.getSelected();
-    // return new OneNoteAuton(shooter, intake);
   }
 
   public void configureBindings() {
-
     // --------------------=Driver=--------------------
+
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
       drivetrain.applyRequest(() -> drive.withVelocityX(driver.getLeftY() * Constants.Drivetrain.MaxSpeed * 0.5) // Drive forward with negative Y (forward)
         .withVelocityY(driver.getLeftX() * Constants.Drivetrain.MaxSpeed * 0.5) // Drive left with negative X (left) //test
         .withRotationalRate(-driver.getRightX() * Constants.Drivetrain.MaxAngularRate) // Drive counterclockwise with negative X (left)
       ));
-
     driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
     driver.b().whileTrue(drivetrain
       .applyRequest(() -> point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
-
     // reset the field-centric heading on left bumper press
     driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
-
     if (Utils.isSimulation()) {
       drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
     }
-    // drivetrain.registerTelemetry(logger::telemeterize);
 
     // --------------------=operator=--------------------
 
-    new Trigger(operator.rightBumper()) // TEST
-      .whileTrue(superstructure.setState(SSStates.SCORE))
-      .whileFalse(superstructure.setState(SSStates.STOWED));
-
+    new Trigger(operator.rightBumper())
+      .whileTrue(new InstantCommand(()-> shooter.setIndexerSpeedScoreSpeaker()));
     new Trigger(operator.x())
       .whileTrue(superstructure.setState(SSStates.WAIT_SPEAKER_SUBWOOFER))
       .whileFalse(superstructure.setState(SSStates.STOWED));
-
-    new Trigger(operator.a())
-      // .whileTrue(
-      //   new SequentialCommandGroup(
-      //     superstructure.setState(SSStates.INTAKE)),
-      //     new WaitCommand(0.5),
-      //     superstructure.setState(SSStates.STOWED));
-
-      .whileTrue(superstructure.setState(SSStates.INTAKE)
-        .andThen(new WaitCommand(2))
-        .until(() -> shooter.beamBroken())
-        .andThen(superstructure.setState(SSStates.INTAKE_BACK)) // DONT WORK
-        .andThen(new WaitCommand(1))
-        .until(() -> shooter.hasNoteRollbackIndexer())
-        // .andThen(superstructure.setState(SSStates.INTAKE_BACK))
-        // .andThen(new WaitCommand(0.8))
-        // .until(() -> shooter.hasNoteRollbackIndexer())
-        .andThen(superstructure.setState(SSStates.STOWED))
-      );
-
-    new Trigger(operator.a())
-      .onFalse(superstructure.setState(SSStates.STOWED));
-      // .whileFalse(superstructure.setState(SSStates.STOWED));
-      
     new Trigger(operator.y())
       .whileTrue(superstructure.setState(SSStates.EJECT_NOTE))
       .whileFalse(superstructure.setState(SSStates.STOWED));
-
     new Trigger(operator.b())
       .whileTrue(superstructure.setState(SSStates.CROSSFIELD))
       .whileFalse(superstructure.setState(SSStates.STOWED));
 
-    // operator.rightBumper().whileTrue(superstructure.setState(SSStates.SCORE)); //
-    // operator.x().whileTrue(superstructure.setState(SSStates.WAIT_SPEAKER_SUBWOOFER));
-    // operator.a().whileTrue(superstructure.setState(SSStates.INTAKE));
-    // operator.button(RobotMap.Controller.MENU_BUTTON).whileTrue(superstructure.setState(SSStates.EJECT_NOTE)); // View button
-    // operator.button(RobotMap.Controller.VIEW_BUTTON).whileTrue(superstructure.setState(SSStates.CROSSFIELD)); // Menu button
+    new Trigger(operator.a())
+      .whileTrue(superstructure.setState(SSStates.INTAKE)
+        .andThen(new WaitUntilCommand(() -> shooter.beamBroken()))
+        .andThen(superstructure.setState(SSStates.STOWED))
+      );
+    new Trigger(operator.a())
+      .onFalse(superstructure.setState(SSStates.INTAKE_BACK)
+        .andThen(new WaitCommand(0.1))
+        .andThen(superstructure.setState(SSStates.STOWED))
+      );
   }
   
   public ShooterPivot getShooterPivot() {
     return shooterPivot;
   }
-
   public Superstructure getSuperstructure() {
     return superstructure;
   }
-
   public void clearPDHStickyFaults() {
     pdh.clearStickyFaults();
   }
-
   public void zeroSuperstructurePositions() {
     shooter.zeroPosition();
     intake.zeroPosition();
     shooterPivot.zeroPosition();
   }
-
   // public Command Command_RumbleControllers()
   //  {
   //      return Commands.runOnce(() ->
@@ -175,4 +127,4 @@ public class RobotContainer {
   //          )
   //      );
   //  }
-}
+  }
--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,11 +1,10 @@
 package frc.robot.subsystems;
-
 import org.ejml.dense.row.decompose.hessenberg.HessenbergSimilarDecomposition_CDRM;
-
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
@@ -13,7 +12,6 @@ import frc.robot.commands.superstructure.IntakeNote.IntakeCommandStates;
 import frc.robot.subsystems.Intake.IntakeStates;
 import frc.robot.subsystems.Shooter.ShooterStates;
 import frc.robot.subsystems.ShooterPivot.ShooterPivotStates;
-
 public class Superstructure extends SubsystemBase {
   public static enum SSStates {
     NONE,
@@ -21,39 +19,31 @@ public class Superstructure extends SubsystemBase {
     INTAKE,
     INTAKE_BACK,
     WAIT_SPEAKER_SUBWOOFER, WAIT_SPEAKER_PODIUM,
-    SCORE,
     EJECT_NOTE,
     CROSSFIELD
   }
   public SSStates currentState = SSStates.NONE;
   public SSStates lastState = SSStates.NONE;
   public SSStates wantedState = SSStates.NONE;
-
   ShooterPivot shooterPivot;
   Shooter shooter;
   Intake intake;
   private Timer timer = new Timer();
-
   public Superstructure(ShooterPivot shooterPivot, Shooter shooter, Intake intake) {
     this.shooterPivot = shooterPivot;
     this.shooter = shooter;
     this.intake = intake;
   }
-
   @Override
   public void periodic() {
     SmartDashboard.putString("Superstructure State [SSS]", currentState.toString());
     handleStateChange();
   }
-
   // Methods
-
   public void handleStateChange() {
     lastState = currentState;
     currentState = wantedState;
-
     switch (currentState) {
-
       case NONE:
       break;
       
@@ -77,152 +67,68 @@ public class Superstructure extends SubsystemBase {
       case WAIT_SPEAKER_PODIUM:
         spinupPod();
       break;
-      
-      case SCORE:
-        score();
-      break;
 
       case EJECT_NOTE:
         ejectNote();
       break;
-
       case CROSSFIELD:
         crossfield();
       break;
     }
   }
-
   // ------ states ------
-
   private void stowed() { //
     shooterPivot.setState(ShooterPivotStates.STOWED);
     shooter.setState(ShooterStates.STANDBY);
     intake.setState(IntakeStates.STOWED);
   }
-
-  private void intake() { // RECODE
-    
-    // intake.setState(IntakeStates.INTAKE);
-    // shooter.setState(ShooterStates.INTAKE_ACCEL);
-    // shooterPivot.setState(ShooterPivotStates.INTAKE);
-
-    // if (shooter.getState() == ShooterStates.INTAKE_ACCEL && waitTimed(0.5)) {
-    //   shooter.setState(ShooterStates.INTAKE);
-
-    //   if (shooter.getState() == ShooterStates.INTAKE && shooter.beamBroken()) {
-    //     intake.setState(IntakeStates.INDEXING);
-    //     shooterPivot.setState(ShooterPivotStates.INDEXING);
-    //     shooter.setState(ShooterStates.INTAKE_ROLLFORWARD);
-
-    //     if (shooter.getState() == ShooterStates.INTAKE_ROLLFORWARD && waitTimed(0.3)) {
-    //       shooter.setState(ShooterStates.HOLD_NOTE);
-          
-    //       if (shooter.getState() == ShooterStates.HOLD_NOTE && waitTimed(0.15)) {
-    //         shooter.setState(ShooterStates.INTAKE_ROLLBACK);
-
-    //         if (shooter.getState() == ShooterStates.INTAKE_ROLLBACK && waitTimed(0.1) && shooter.hasNoteRollbackIndexer()) {
-    //           return;
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
+  private void intake() {
 
     intake.setState(IntakeStates.INTAKE);
     shooter.setState(ShooterStates.INTAKE);
     shooterPivot.setState(ShooterPivotStates.INTAKE);
-
-    // if (timer.hasElapsed(0.5)) {
-    //   System.out.println("INTAKING INTAKING INTAKING");
-    //   shooter.setState(ShooterStates.INTAKE);
-      
-    //   if (shooter.getState() == ShooterStates.INTAKE && shooter.beamBroken()) {
-    //     System.out.println("FORWARD FORWARD FORWARD");
-    //     intake.setState(IntakeStates.INDEXING);
-    //     // shooterPivot.setState(ShooterPivotStates.INDEXING);
-    //     shooter.setState(ShooterStates.INTAKE_ROLLFORWARD);
-
-    //     if (shooter.getState() == ShooterStates.INTAKE_ROLLFORWARD && shooter.hasDetectedNoteShooter() && timer.hasElapsed(0.3)) {
-    //       shooter.setState(ShooterStates.HOLD_NOTE);
-
-    //       if (shooter.getState() == ShooterStates.HOLD_NOTE && timer.hasElapsed(0.15)) {
-    //         shooter.setState(ShooterStates.INTAKE_ROLLBACK);
-
-    //         if (shooter.getState() == ShooterStates.INTAKE_ROLLBACK && shooter.hasNoteRollbackIndexer() && timer.hasElapsed(0.1)) {
-    //           return;
-    //         }
-    //       }
-    //     }
+  }
+  private void intakeBack() {
+    shooter.setIndexerRollBack();
+  }
+  private void spinupSub() { //
+    // System.out.println("SUROIAHFOADHOFAHEUOIFAHFUIJ");
+    shooter.setState(ShooterStates.SPINUP_SUBWOOFER);
+    shooterPivot.setState(ShooterPivotStates.SPEAKER_SUBWOOFER);
+    intake.setState(IntakeStates.SCORE_SPEAKER_SUBWOOFER);
+    // if (shooter.atGoalShooter() && timer.hasElapsed(0.5)) {
+    //   shooter.setIndexerSpeedScoreSpeaker();
+    //   if (timer.hasElapsed(0.5)) {
+    //     return;
     //   }
     // }
   }
 
-  private void intakeBack() {
-    shooter.setIndexerRollBack();
-  }
-
-  private void spinupSub() { //
-    timer.start();
-
-    shooter.setState(ShooterStates.SPINUP_SUBWOOFER);
-    shooterPivot.setState(ShooterPivotStates.SPEAKER_SUBWOOFER);
-    intake.setState(IntakeStates.SCORE_SPEAKER_SUBWOOFER);
-
-    if (shooter.atGoalShooter() && timer.hasElapsed(0.5)) {
-      shooter.setIndexerSpeedScoreSpeaker();
-
-      if (timer.hasElapsed(0.5)) {
-        return;
-      }
-    }
-  }
-
   private void spinupPod() { //
     shooter.setState(ShooterStates.SPINUP_PODIUM);
-    shooterPivot.setState(ShooterPivotStates.SPEAKER_PODIUM);
-    intake.setState(IntakeStates.STOWED);
-
-    if (shooter.atGoalShooter() && timer.hasElapsed(0.3)) {
-      shooter.setIndexerSpeedScoreSpeaker();
-
-      if (waitTimed(0.5)) {
-        return;
-      }
+    intake.setState(IntakeStates.SCORE_SPEAKER_PODIUM);
+    if (timer.hasElapsed(1)) {
+        shooterPivot.setState(ShooterPivotStates.SPEAKER_PODIUM);
     }
-  }
-
-  private void score() { // make sure pivot stays in correct position
-
-    if (shooter.atGoalShooter() && waitTimed(0.3)) {
-      shooter.setIndexerSpeedScoreSpeaker();
-    }
-    // shooterPivot.setState(ShooterPivotStates.SPEAKER_SUBWOOFER);
-    // shooter.setState(ShooterStates.SCORE);
-    intake.setState(IntakeStates.STOWED);
   }
 
   private void ejectNote() { //
     intake.setState(IntakeStates.EJECT_NOTE);
     if (timer.hasElapsed(0.5)) {
       shooterPivot.setState(ShooterPivotStates.EJECT_NOTE);
-
       if (timer.hasElapsed(1)){
         shooter.setState(ShooterStates.EJECT_NOTE);
       }
     }
   }
-
   private void crossfield() {
     intake.setState(IntakeStates.CROSSFIELD);
     shooter.setState(ShooterStates.SPINUP_CROSSFIELD);
-
-    if (timer.hasElapsed(0.2)) { // delay for intake move out
+    if (timer.hasElapsed(1)) { // delay for intake move out
       shooterPivot.setState(ShooterPivotStates.CROSSFIELD);
     }
   }
-
   // ------ commands -------
-
   /**
    * Sets the superstructure target state
    * @param currentState Target state
@@ -230,14 +136,7 @@ public class Superstructure extends SubsystemBase {
   public Command setState(SSStates wantedState) {
       return new InstantCommand(() -> this.wantedState = wantedState);
   }
-
   // ------ methods ------
-
-  public boolean waitTimed(double duration) {
-    Timer timer = new Timer();
-    System.out.println("WAITTIMERWAITTIMER/nWAITTIMERWAITTIMER/nWAITTIMERWAITTIMER/nWAITTIMERWAITTIMER/nWAITTIMERWAITTIMER/n");
-    return timer.get() > duration;
-  }
 
   public SSStates getCurrentState() {
       return currentState;
