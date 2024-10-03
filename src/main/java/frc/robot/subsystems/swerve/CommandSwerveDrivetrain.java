700a86761ec393e7975e01689c0a368ecb12ad4b
Eric Wang <ewang105105@gmail.com>
2024-10-02 17:35:51 -0700

    more pathplanner implementation still broken
    references named commands that dont exist

--- src/main/java/frc/robot/subsystems/swerve/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/swerve/CommandSwerveDrivetrain.java
@@ -2,45 +2,49 @@ package frc.robot.subsystems.swerve;
 
 import java.util.function.Supplier;
 
+import static edu.wpi.first.units.Units.Volts;
+
+
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
-import edu.wpi.first.math.MathUtil;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-
-// import com.pathplanner.lib.auto.AutoBuilder;
-// import com.pathplanner.lib.commands.PathPlannerAuto;
-// import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
-// import com.pathplanner.lib.util.PIDConstants;
-// import com.pathplanner.lib.util.ReplanningConfig;
-
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants;
-import frc.robot.subsystems.Vision;
-import frc.util.LimelightHelper.PoseEstimate;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements
  * subsystem so it can be used in command-based projects easily.
  */
 public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsystem {
-
     private static final double kSimLoopPeriod = 0.005; // 5 ms
     private Notifier m_simNotifier = null;
     private double m_lastSimTime;
+
+    PIDConstants PP_PID_Translation = new PIDConstants(0.25, 0, 0); //0.25, 0, 0
+    PIDConstants PP_PID_Rotation = new PIDConstants(1.85, 0, 0.65); //1.85, 0, 0.65
+    
 
     /* Blue alliance sees forward as 0 degrees (toward red alliance wall) */
     private final Rotation2d BlueAlliancePerspectiveRotation = Rotation2d.fromDegrees(0);
@@ -49,22 +53,15 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
     /* Keep track if we've ever applied the operator perspective before or not */
     private boolean hasAppliedOperatorPerspective = false;
 
-    // public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
-    //     super(driveTrainConstants, OdometryUpdateFrequency, modules);
-    //     if (Utils.isSimulation()) {
-    //         startSimThread();
-    //     }
-    // }
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
-        super(driveTrainConstants, modules);
-        if (Utils.isSimulation()) {
-            startSimThread();
-        }
-    }
+    private final SwerveRequest.ApplyChassisSpeeds AutoRequest = new SwerveRequest.ApplyChassisSpeeds();
 
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
         return run(() -> this.setControl(requestSupplier.get()));
     }
+
+    public Command getAutoPath(String pathName) {
+        return new PathPlannerAuto(pathName);
+    } 
 
     private void startSimThread() {
         m_lastSimTime = Utils.getCurrentTimeSeconds();
@@ -81,25 +78,67 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
         m_simNotifier.startPeriodic(kSimLoopPeriod);
     }
 
-    public void configurePathPlanner(){}
+    public ChassisSpeeds getCurrentRobotChassisSpeeds() {
+        return m_kinematics.toChassisSpeeds(getState().ModuleStates);
+    }
+
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
+        super(driveTrainConstants, OdometryUpdateFrequency, modules);
+        configurePathPlanner();
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+    }
+
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
+        super(driveTrainConstants, modules);
+        configurePathPlanner();
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+    }
+
+    private void configurePathPlanner(){
+
+        double driveBaseRadius = 0; //meters is 0.3334, 13.125
+        for (var moduleLocation : m_moduleLocations) {
+            driveBaseRadius = Math.max(driveBaseRadius, moduleLocation.getNorm());
+        }
+
+        
+
+        AutoBuilder.configureHolonomic(
+            () -> this.getState().Pose, 
+            this::seedFieldRelative, 
+            this::getCurrentRobotChassisSpeeds, 
+            (speeds) -> this.setControl(AutoRequest.withSpeeds(speeds)), 
+            new HolonomicPathFollowerConfig(PP_PID_Translation, //Tune 
+                                            PP_PID_Rotation, 
+                                            Constants.Drivetrain.kSpeedAt12VoltsMps, 
+                                            driveBaseRadius, 
+                                            new ReplanningConfig()), 
+            () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red, 
+            this);
+    }
 
     public Pose2d getPose() {
         return this.m_odometry.getEstimatedPosition();
+    }
+
+    public Rotation2d getYaw() {
+        return new Rotation2d(Math.toRadians(getPigeon2().getAngle()));
+    }
+
+    public void updateOdometry(Pose2d pose) {
+        this.m_odometry.resetPosition(getYaw(), m_modulePositions, pose);
     }
 
     public Translation3d getTranslation3d() {
         return new Translation3d(getPose().getX(), getPose().getY(), 0.0);
     }
 
-    public double getHeading() {
-        double angle = this.m_pigeon2.getRotation2d().plus(Rotation2d.fromDegrees(180)).getRadians(); 
-        angle *= -1.0;
-        return angle;
-    }
-
     @Override
     public void periodic() {
-        debug();
         /* Periodically try to apply the operator perspective */
         /* If we haven't applied the operator perspective before, then we should apply it regardless of DS state */
         /* This allows us to correct the perspective in case the robot code restarts mid-match */
@@ -113,9 +152,6 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
                 hasAppliedOperatorPerspective = true;
             });
         }
-    }
-
-    private void debug() {
-        SmartDashboard.putNumber("pose X", this.m_odometry.getEstimatedPosition().getX());
+        
     }
 }
